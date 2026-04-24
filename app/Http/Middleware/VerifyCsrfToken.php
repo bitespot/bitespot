2c2bee8443bc54589d3d26f14ffe498c053a7860
rolpppp <rlgarces@up.edu.ph>
2026-04-24 15:06:04 +0800

    feat: added auth controllers

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class VerifyCsrfToken
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return $next($request);
+    }
+
+    protected $except = [
+        'api/test',
+    ];
+}
