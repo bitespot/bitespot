f468eb0c4b7efec9369d16246fd77e24dba9106e
normaneulin <eulinnormanenricocatalan@gmail.com>
2026-05-08 01:12:32 +0800

    chore: minor revision on icons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -119,7 +119,7 @@
                 </p>
 
                 <div class="bs-search w-full max-w-xl mx-auto flex flex-col sm:flex-row gap-2 sm:gap-0" id="hero-search-bar">
-                    <div class="bs-search__inner flex-1 min-w-0 px-2 py-1 sm:px-0 sm:py-0">
+                    <div class="bs-search__inner flex-1 min-w-0 px-2 py-1 sm:px-4 sm:py-0">
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"
                              fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-1">
                             <circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/>
